f0419cb9f03f3cc3827494130852591a227101b5
Fredrik Dokka Nordstad <fredrikdn@gmail.com>
2019-11-03 16:46:43 +0100

    Header and footer now fully dynamic Works with mobile and desktop now  Changes to be committed: 	deleted:    css/formstyle.css 	modified:   css/style.css 	modified:   feedback.html 	new file:   filmquiz.html 	modified:   index.html 	modified:   js/scripts.js 	modified:   resources/logo.png

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
+// clone header and footer
+function clone() {
+    var div = document.getElementsByClassName('header'),
+    clone = div.cloneNode(true);
+    clone.before(document.getElementsByClassName('content'));
+}
+
 // curtain menu on the right, listener
 const iconMenu = document.querySelector('.menu_toggler');
 const menu = document.querySelector('.menu');
 
-iconMenu.addEventListener('click', function() {
+iconMenu.addEventListener('click', function () {
     this.classList.toggle('active');
     menu.classList.toggle('active');
 })
@@ -10,14 +17,14 @@ iconMenu.addEventListener('click', function() {
 
 // form to json, handle filmtip-information submission
 const formToJson = elements => [].reduce.call(elements, (data, element) => {
-    
+
     data[element.name] = element.value;
     return data;
 }, {});
 
-const handleFormSubmit = event => { 
+const handleFormSubmit = event => {
     event.preventDefault();
-    
+
     const data = formToJson(form.elements);
     // this part of the handleFormSubmit should be handled with node.js to write
     // to a file on server-side, while this part only shows how the JSON-output would 
@@ -25,5 +32,5 @@ const handleFormSubmit = event => {
     const dataContainer = document.getElementsByClassName('output')[0];
     dataContainer.textContent = JSON.stringify(data, null, " ");
 };
-    const form = document.getElementsByClassName('form')[0];
-    form.addEventListener('submit', handleFormSubmit);
+const form = document.getElementsByClassName('form')[0];
+form.addEventListener('submit', handleFormSubmit);

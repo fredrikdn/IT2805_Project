c06969a48af05cfed27638b058864ba94afb13f4
Fredrik Dokka Nordstad <fredrikdn@gmail.com>
2019-11-03 14:54:03 +0100

    Added submit form for film showings Finished form for handling film-tips, writes to JSON format. Not able to create a doc with json-objects.  Changes to be committed: 	new file:   css/formstyle.css 	modified:   css/style.css 	new file:   feedback.html 	modified:   index.html 	modified:   js/scripts.js 	modified:   visninger.html

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,24 @@ iconMenu.addEventListener('click', function() {
     this.classList.toggle('active');
     menu.classList.toggle('active');
 })
+
+
+// form to json, handle filmtip-information submission
+const formToJson = elements => [].reduce.call(elements, (data, element) => {
+    
+    data[element.name] = element.value;
+    return data;
+}, {});
+
+const handleFormSubmit = event => { 
+    event.preventDefault();
+    
+    const data = formToJson(form.elements);
+    // this part of the handleFormSubmit should be handled with node.js to write
+    // to a file on server-side, while this part only shows how the JSON-output would 
+    // look like on the bottom of the page
+    const dataContainer = document.getElementsByClassName('output')[0];
+    dataContainer.textContent = JSON.stringify(data, null, " ");
+};
+    const form = document.getElementsByClassName('form')[0];
+    form.addEventListener('submit', handleFormSubmit);
